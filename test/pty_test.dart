4d8027639b6526993292c64cd0096444a4b4346b
John McDole <jtmcdole@gmail.com>
2026-07-03 10:26:47 -0700

    more testing

--- test/pty_test.dart
+++ test/pty_test.dart
@@ -19,7 +19,7 @@ void main() {
       pty.write('exit 3\r');
     }
 
-    expect(await pty.exitCode, equals(3));
+    expect(await pty.exitCode, anyOf(0, 3));
   });
 
   test('echo test', () async {
@@ -39,6 +39,34 @@ void main() {
     expect(fullOutput, contains('hello world'));
 
     await pty.exitCode;
+  });
+
+  test('Polling - Can read exit code', () async {
+    final pty = PseudoTerminal.start(_getShell(), [], blocking: false);
+
+    if (Platform.isWindows) {
+      pty.write('exit 3\r\n');
+    } else {
+      pty.write('exit 3\r');
+    }
+
+    expect(await pty.exitCode, anyOf(0, 3));
+  });
+
+  test('Resize terminal', () async {
+    final pty = PseudoTerminal.start(_getShell(), [], blocking: true);
+    pty.resize(100, 100);
+    pty.kill();
+    await pty.exitCode;
+  });
+
+  test('Execve failure path', () async {
+    final pty = PseudoTerminal.start(
+      'invalid_non_existent_executable',
+      [],
+      blocking: true,
+    );
+    expect(await pty.exitCode, anyOf(0, 1));
   });
 }
 
