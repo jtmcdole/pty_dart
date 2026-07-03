25ca087add8ebad284986f15eb0fca8288cd8142
jtmcdole <john@mcdole.org>
2026-07-03 09:29:31 -0700

    missing file

---
+++ lib/pty2.dart
@@ -0,0 +1,77 @@
+import 'dart:io';
+
+import 'package:pty2/src/impl/unix.dart';
+import 'package:pty2/src/impl/windows.dart';
+import 'package:pty2/src/pty.dart';
+import 'package:pty2/src/pty_core.dart';
+
+export 'src/pty.dart';
+
+abstract class PseudoTerminal {
+  /// If [blocking] is [true], the PseudoTerminal starts in blocking mode
+  /// (better suited for flutter release mode), otherwise in polling mode
+  /// (better suited for flutter debug mode).
+  static PseudoTerminal start(
+    String executable,
+    List<String> arguments, {
+    String? workingDirectory,
+    Map<String, String>? environment,
+    bool blocking = false,
+    bool ackProcessed = false,
+    // bool includeParentEnvironment = true,
+    // bool runInShell = false,
+    // ProcessStartMode mode = ProcessStartMode.normal,
+  }) {
+    late PtyCore core;
+
+    if (Platform.isWindows) {
+      blocking = true;
+      core = PtyCoreWindows.start(
+        executable,
+        arguments,
+        workingDirectory: workingDirectory,
+        environment: environment,
+        blocking: blocking,
+      );
+    } else {
+      //add '-l' as argument for the shell to perform a login
+      arguments = List<String>.generate(
+        arguments.length + 1,
+        (index) => index == 0 ? '-l' : arguments[index - 1],
+      );
+
+      core = PtyCoreUnix.start(
+        executable,
+        arguments,
+        workingDirectory: workingDirectory,
+        environment: environment,
+        blocking: blocking,
+      );
+    }
+
+    final BasePseudoTerminal pty;
+    if (blocking) {
+      pty = BlockingPseudoTerminal(core, ackProcessed);
+    } else {
+      pty = PollingPseudoTerminal(core);
+    }
+    return pty..init();
+  }
+
+  void init();
+  bool kill([ProcessSignal signal = ProcessSignal.sigterm]);
+
+  Future<int> get exitCode;
+
+  // int get pid {
+  //   return _core.pid;
+  // }
+
+  void write(String input);
+
+  Stream<String> get out;
+
+  void ackProcessed();
+
+  void resize(int width, int height);
+}
